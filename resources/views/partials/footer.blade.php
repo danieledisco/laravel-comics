9173efe9697ca444fa64e143bcf4fe4ec559ba55
danieledisco <daniele.disco.1965@gmail.com>
2024-04-16 23:20:39 +0200

    Completati (o quasi) Header e Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,84 @@
 <footer>
-    Footer
+    <div class="footerUp">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+
+                    <h2>DC COMICS</h2>
+                    <ul class="listClass">
+                        <li><a href="#">Characters</a> </li>
+                        <li><a href="#">Comics</a> </li>
+                        <li><a href="#">Movies</a> </li>
+                        <li><a href="#">TV</a> </li>
+                        <li><a href="#">Games</a> </li>
+                        <li><a href="#">Videos</a> </li>
+                        <li><a href="#">News</a> </li>
+                    </ul>
+
+                    <h2>SHOP</h2>
+                    <ul class="listClass">
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h2>DC</h2>
+                    <ul class="listClass">
+                        <li><a href="#">Terms Of Use</a> </li>
+                        <li><a href="#">Provacy policy (New)</a> </li>
+                        <li><a href="#">Ad Chocices</a> </li>
+                        <li><a href="#">Advertising</a> </li>
+                        <li><a href="#">Jobs</a> </li>
+                        <li><a href="#">Subscriptions</a> </li>
+                        <li><a href="#">CPSC Cetificates</a> </li>
+                        <li><a href="#">Ratings</a> </li>
+                        <li><a href="#">Shop Help</a> </li>
+                        <li><a href="#">Contact Us</a> </li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h2>SITES</h2>
+                    <ul class="listClass">
+                        <li><a href="#">DC</a> </li>
+                        <li><a href="#">MAS Magazie</a> </li>
+                        <li><a href="#">DC Kids</a> </li>
+                        <li><a href="#">DC Universe</a> </li>
+                        <li><a href="#">DC Power Visa</a> </li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="informations">
+                All Site Content TM and &copy; 2020 DC Entertainement, unless otherwise <span>noted here</span>. All
+                right reserved.<br>
+                <span>Cokies Settings</span>
+            </div>
+        </div>
+    </div>
+    <div class="footerDown">
+        <div class="container">
+            <div class="contELements">
+                <div class="sign">
+                    <button class="btn">SIGN-UP NOW!</button>
+                </div>
+                <nav>
+                    <ul class="listClass">
+                        <li class="firstLi">FOLLOW US</li>
+                        <li><a href="#"><img src="{{ Vite::asset('resources/images/footer-facebook.png') }}"></a>
+                        </li>
+                        <li><a href="#"><img src="{{ Vite::asset('resources/images/footer-twitter.png') }}"></a>
+                        </li>
+                        <li><a href="#"><img src="{{ Vite::asset('resources/images/footer-youtube.png') }}"></a>
+                        </li>
+                        <li><a href="#"><img src="{{ Vite::asset('resources/images/footer-pinterest.png') }}"></a>
+                        </li>
+                        <li><a href="#"><img src="{{ Vite::asset('resources/images/footer-periscope.png') }}"></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,45 @@
 <header>
-    Header
+    <div class="current">
+        CURRENT SERIES
+    </div>
+    <div class="littleBorder">
+        <div class="container">
+            <div class="firstBox">
+                DC POWER<sup>SM</sup> VISA
+            </div>
+            <div class="secondBox">
+                ADDITIONAL DC SITES
+            </div>
+        </div>
+    </div>
+    <div class="contElements">
+        <div class="container">
+            <div class="logo">
+                <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" width="50">
+            </div>
+            <nav>
+                <ul class="main_menu">
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#" class="active">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+            <div class="search">
+                Search
+            </div>
+        </div>
+
+    </div>
+    <div class="jumbotron">
+        <img src="{{ Vite::asset('resources/images/jumbotron.jpg') }}">
+    </div>
+
+
 </header>
